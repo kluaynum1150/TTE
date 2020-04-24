2ef074f32e5d465adee584a3ef08ad2ca737d142
kluaynum1150 <kluaynum951@gmail.com>
2020-04-24 17:13:51 +0700

    master

--- TTE.js
+++ TTE.js
@@ -7,6 +7,19 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/logout", function(req, res){
+    req.logOut();
+    res.redirect("/");
+});
+
+app.get("/signup", function(req, res){
+    res.render("signup");
+});
+
 app.listen(3000,function(){
     console.log('Server is started');
 });
