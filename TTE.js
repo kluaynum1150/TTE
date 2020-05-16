4fcad5a9e5f766c285188b86dba24f73ab3a0c55
kluaynum1150 <kluaynum951@gmail.com>
2020-05-16 18:48:25 +0700

    login0.5+signup0.5

--- TTE.js
+++ TTE.js
@@ -1,24 +1,42 @@
-const express = require("express");
-let app = express();
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      flash = require('connect-flash'),
+      passport = require('passport'),
+      passportLocal = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      user = require('./models/user'),
+      indexRoutes = require('./routes/index');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/tteDatabase', {useNewUrlParser: true});
 app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(flash());
 
-app.get("/", function(req, res){
-    res.render("landing");
+app.use(require("express-session")({
+    secret: 'tte',
+    resave: false,
+    saveUninitialized: false
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
 });
 
-app.get("/login", function(req, res){
-    res.render("login");
-});
+passport.use(new passportLocal(user.authenticate()));
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
 
-app.get("/logout", function(req, res){
-    req.logOut();
-    res.redirect("/");
-});
-
-app.get("/signup", function(req, res){
-    res.render("signup");
-});
+app.use("/",indexRoutes);
 
 app.listen(3000,function(){
     console.log('Server is started');
