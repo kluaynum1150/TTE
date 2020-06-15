0ab80396ab783755ed148aa9d51eb5964d92a3a2
kluaynum1150 <kluaynum951@gmail.com>
2020-06-16 02:05:16 +0700

    add edit delete map

--- routes/adminPage.js
+++ routes/adminPage.js
@@ -39,8 +39,57 @@ router.post("/addCheckpoint", middleware.isLoggedInAdmin, function(req,res){
             console.log(err);
             res.redirect("/admin");
         } else{
-            console.log(newCheckpoint);
+            req.flash("success","Add a new checkpoint success.");
             res.redirect("/admin");
+        }
+    });
+});
+
+//find all checkpoint
+router.get("/editOrDeleteCheckpoint", middleware.isLoggedInAdmin, function(req,res){
+    map.find({},function(err,found){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("adminPage/chooseCheckpoint",{allMap:found});
+        }
+    });
+});
+
+//edit checkpoint
+router.get("/editCheckpoint/:map_id", middleware.isLoggedInAdmin, function(req,res){
+    map.findById(req.params.map_id, function(err,foundMap){
+        if(err){
+            console.log(err);
+            res.redirect("/admin/editOrDeleteCheckpoint")
+        } else{
+            res.render("adminPage/editCheckpoint",{info_map:foundMap});
+        }
+    });
+});
+
+router.put("/editCheckpoint/:map_id", middleware.isLoggedInAdmin, function(req,res){
+    let n_info = {name: req.body.nameCheckpoint, level: req.body.level, information: req.body.information};
+    map.findByIdAndUpdate(req.params.map_id, n_info, function(err,updated){
+        if(err){
+            console.log(err);
+            res.redirect("/admin");
+        } else{
+            req.flash("success","Edit a checkpoint success.");
+            res.redirect("/admin/editOrDeleteCheckpoint");
+        }
+    });
+});
+
+//delete checkpoint
+router.delete("/deleteCheckpoint/:map_id", middleware.isLoggedInAdmin, function(req,res){
+    map.findByIdAndRemove(req.params.map_id, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/admin");
+        } else{
+            req.flash("success","Delete a checkpoint success.");
+            res.redirect("/admin/editOrDeleteCheckpoint");
         }
     });
 });
